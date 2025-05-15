2ed53c598890d283a6c27373564806bdacb25ff1
Jiexxxxxx11 <jessexxxx37@gmail.com>
2025-05-16 00:31:51 +1000

    add icons and relate css files

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+// menu.js
+const menuBtn = document.getElementById('menuIcon');
+const sideMenu = document.getElementById('sideMenu');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.getElementById('closeMenu');
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.classList.add('active');
+  overlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  sideMenu.classList.remove('active');
+  overlay.classList.remove('active');
+});
